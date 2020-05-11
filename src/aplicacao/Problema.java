2d2e602741bc519dc1b6d608ac1f2407e4b3bfdb
Andre Vilela <andrevibegoogle@hotmail.com>
2020-05-11 16:46:28 -0300

    Consolidado de renda

--- src/aplicacao/Problema.java
+++ src/aplicacao/Problema.java
@@ -11,7 +11,7 @@ public class Problema {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		double salario, prestacaoDeServico, ganhoDeCapital, gastosMedicos, gastosEducacionais;
+		double salario, prestacaoDeServico, ganhoDeCapital, gastosMedicos, gastosEducacionais, impostoSalario, impostoServicos, impostoGanhoDeCapital;
 		
 		System.out.println("Renda anual com salário: ");
 		salario = sc.nextDouble();
@@ -23,6 +23,34 @@ public class Problema {
 		gastosMedicos = sc.nextDouble();
 		System.out.println("Gastos educacionais: ");
 		gastosEducacionais = sc.nextDouble();
+		
+		if ((salario / 12) < 3000) {
+			impostoSalario = 0;
+		} else if (((salario / 12) >= 3000.00) && ((salario / 12) < 5000.00)) {
+			impostoSalario = salario * 0.10;
+		} else {
+			impostoSalario = salario * 0.20;
+		}
+		
+		if (prestacaoDeServico > 0) {
+			impostoServicos = prestacaoDeServico * 0.15;
+		} else {
+			impostoServicos = 0;
+		}
+		
+		if (ganhoDeCapital > 0) {
+			impostoGanhoDeCapital = ganhoDeCapital * 0.20;
+		} else {
+			impostoGanhoDeCapital = 0;
+		}
+		
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA");
+		System.out.println();
+		System.out.println("CONSOLIDADO DE RENDA:");
+		System.out.printf("Imposto sobre salário: %.2f%n", impostoSalario);
+		System.out.printf("Imposto sobre serviços: %.2f%n", impostoServicos);
+		System.out.printf("Imposto sobre ganho de capital: %.2f%n", impostoGanhoDeCapital);
 
 	}
 
